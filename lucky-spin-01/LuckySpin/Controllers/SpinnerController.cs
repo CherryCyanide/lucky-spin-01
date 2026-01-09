3bc42dfba571b0a52ddf94cf50f36b8cc5bafead
CherryCyanide <isobellwaite@student.cascadia.edu>
2026-01-09 14:19:56 -0800

    Manual Upload, issues with git
    Completed parts 4, 5, and 6

---
+++ lucky-spin-01/LuckySpin/Controllers/SpinnerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuckySpin.Controllers
+{
+    public class SpinnerController : Controller
+    {
+
+        public IActionResult Index(int luck) 
+        {
+            //DONE: add your string builder and HTML from Exercise 0 here
+            int[] spin = new int[] { new Random().Next(10), new Random().Next(10), new Random().Next(10) };
+
+            System.Text.StringBuilder htmlToShow =  
+            new System.Text.StringBuilder("<body><h1> </h1><button onclick='history.go(0)'>Spin</button>");
+            htmlToShow.Append("<div>" + spin[0] + "</div>");
+            htmlToShow.Append("<div>" + spin[1] + "</div>");
+            htmlToShow.Append("<div>" + spin[2] + "</div>");
+            foreach (int num in spin) 
+                {
+                    if (num == luck)
+                    {
+                        htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");
+                        break;
+                    }
+                } 
+                
+
+            //DONE: Modify this to use the string builder's response string as the Content property's value
+            return new ContentResult { Content = $"<h1>We're Ready to Spin with Lucky Number {luck} </h1>" + htmlToShow.ToString(), ContentType="text/html"};
+        }
+    }
+}
---
+++ lucky-spin-01/LuckySpin/Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Install Services using the builder.Services methods
+  //  DONE: include the builder service "AddControllers" to enable MVC controllers
+builder.Services.AddControllers();
+
+//Builds the app with the added services
+var app = builder.Build();
+
+
+// Configure the app's Middleware in the HTTP Request Pipeline
+//   DONE: The following code "app.UseStaticFiles()" provides the ability to recognize static folders and files in the wwwroot directory
+app.UseStaticFiles();
+
+//   DONE: The following code "app.UseExceptionHandler" provides a default error page when not in development
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Spinner/Error");
+}
+//   DONE: include the app method "UseRouting" to recognize custom "Routes" in place of folders and files
+app.UseRouting();
+
+
+//Configure Routing with a general pattern and a default setting if the URL path is left out
+ // DONE:if your have time, add a range(1,9) method to constrain luck between 1 and 9
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{luck:range(1,9)}",
+    defaults: new
+    {
+        controller = "Spinner",
+        action = "Index",
+        luck = 7
+    });
+
+app.Run();
+
+// WAWAWAWAWAWAWAWA
